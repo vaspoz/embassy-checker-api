5092ad3ce674d0657f20a874ee1380385922e892
vaspoz <vaspoz@mail.ru>
2020-09-20 20:09:59 +0200

    refactored

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 #export GOOGLE_APPLICATION_CREDENTIALS="C:\Users\Administrator\Downloads\github\embassy-scheduler.json"
 export GOOGLE_APPLICATION_CREDENTIALS="C:\Users\vaspo\github\embassy-scheduler-vision.json"
-node component/index.js
+node components/index.js
