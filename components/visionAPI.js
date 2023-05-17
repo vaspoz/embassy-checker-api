83fd37f09f13fd5718eca0157bc4edd4c3cf58dc
Basil Pozdeev <basil.pozdeev@ctac.nl>
2023-05-17 14:50:24 +0200

    Removed unused files

--- components/visionAPI.js
+++ components/visionAPI.js
@@ -1,33 +1,35 @@
-const request = require('request').defaults({encoding: null});
-const fs = require('fs');
-const vision = require('@google-cloud/vision');
+const request = require("request").defaults({ encoding: null });
+const fs = require("fs");
+const vision = require("@google-cloud/vision");
+const path = require("path");
 
-module.exports.fromLink = (path, callback) => {
+module.exports.fromLink = (link, callback) => {
+	if (!link) {
+		link = "http://hague.kdmid.ru/queue/CodeImage.aspx?id=c646";
+	}
 
-    if (!path) {
-        path = 'http://hague.kdmid.ru/queue/CodeImage.aspx?id=c646';
-    }
+	request.get(link, function (error, response, body) {
+		let base64Image = new Buffer.from(body).toString("base64");
+		fs.writeFile(
+			path.join(__dirname, "images/magicNumbers.png"),
+			base64Image,
+			{ encoding: "base64" },
+			() => {
+				const client = new vision.ImageAnnotatorClient();
+				const fileName = "components/images/magicNumbers.png";
 
-    request.get(path, function (error, response, body) {
-        let base64Image = new Buffer.from(body).toString('base64');
-        fs.writeFile('components/images/magicNumbers.png', base64Image, {encoding: 'base64'}, () => {
-
-            const client = new vision.ImageAnnotatorClient();
-            const fileName = 'components/images/magicNumbers.png';
-
-            client.textDetection(fileName)
-                .then(([result]) => {
-                    callback(result.textAnnotations[0].description);
-                });
-        });
-    });
+				client.textDetection(fileName).then(([result]) => {
+					callback(result.textAnnotations[0].description);
+				});
+			}
+		);
+	});
 };
 
 module.exports.fromFile = (fileName, callback) => {
-    const client = new vision.ImageAnnotatorClient();
+	const client = new vision.ImageAnnotatorClient();
 
-    client.textDetection(fileName)
-        .then(([result]) => {
-            callback(result.textAnnotations[0].description);
-        });
+	client.textDetection(fileName).then(([result]) => {
+		callback(result.textAnnotations[0].description);
+	});
 };
