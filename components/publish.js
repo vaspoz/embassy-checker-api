1ba0e41653394360f42a4a398c959d73a09b446f
vaspoz <vaspoz85@gmail.com>
2024-01-24 16:10:52 +0100

    [MessageBird] Turn off the messaging if the key is not provided

--- components/publish.js
+++ components/publish.js
@@ -1,23 +1,35 @@
-const messagebird = require("messagebird").initClient(
-	process.env.MESSAGEBIRD_API_KEY
-);
+let messagebird;
 const config = require("../config.json");
 
 module.exports = (text) => {
-	if (config.sendSMS) sendSMS(text);
+    if (config.sendSMS) sendSMS(text);
+};
+
+const initMessageBird = () => {
+    if (messagebird) return;
+    if (!process.env.MESSAGEBIRD_API_KEY) {
+        console.log("[MessageBird] Couldn't find the api key in env variable, sms won't be send.");
+        throw new Error();
+    }
+    messagebird = require("messagebird").initClient(process.env.MESSAGEBIRD_API_KEY);
 };
 
 const sendSMS = (text) => {
-	const params = {
-		originator: "+31638643409",
-		recipients: config.smsRecipients,
-		body: text
-	};
+    try {
+        initMessageBird();
+    } catch {
+        return;
+    }
+    const params = {
+        originator: "+31638643409",
+        recipients: config.smsRecipients,
+        body: text,
+    };
 
-	messagebird.messages.create(params, function (err, response) {
-		if (err) {
-			return console.log(err);
-		}
-		console.log(response);
-	});
+    messagebird.messages.create(params, function (err, response) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(response);
+    });
 };
